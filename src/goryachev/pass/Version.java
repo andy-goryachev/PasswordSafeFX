c617fde294f66105c0e77e5cef33c79c2af766d2
Andy Goryachev <andy@goryachev.com>
2016-12-25 07:44:25 -0800

    copy crypto from the parent project

---
+++ src/goryachev/pass/Version.java
@@ -0,0 +1,11 @@
+// Copyright © 2016 Andy Goryachev <andy@goryachev.com>
+package goryachev.pass;
+
+
+/**
+ * Version.
+ */
+public class Version
+{
+	public static final String VERSION   = "3.04.47";
+}
---
+++ src/goryachev/pass/data/DataFormat.java
@@ -0,0 +1,11 @@
+// Copyright © 2013-2016 Andy Goryachev <andy@goryachev.com>
+package goryachev.pass.data;
+import goryachev.crypto.OpaqueChars;
+
+
+public interface DataFormat
+{
+	public byte[] save(DataFile f) throws Exception;
+	
+	public DataFile load(byte[] encrypted, OpaqueChars passphrase) throws Exception;
+}
