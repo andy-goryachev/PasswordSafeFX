bd22dab13fbb2b04d802ac3979140320a2eced07
Andy Goryachev <andy@goryachev.com>
2016-12-25 09:36:24 -0800

    zero string

---
+++ src/goryachev/common/util/CField.java
@@ -0,0 +1,65 @@
+// Copyright © 2016 Andy Goryachev <andy@goryachev.com>
+package goryachev.common.util;
+import java.lang.reflect.Field;
+
+
+/**
+ * A "typesafe" java.lang.reflect.Field equivalent that does not throw Exceptions.
+ */
+public final class CField<T>
+{
+	private final Field field;
+	
+	
+	public CField(Class c, String name)
+	{
+		this.field = init(c, name);
+	}
+	
+	
+	private static Field init(Class c, String name)
+	{
+		try
+		{
+			return c.getDeclaredField(name);
+		}
+		catch(Throwable e)
+		{
+			Log.ex(e);
+			return null;
+		}
+	}
+	
+	
+	public T get(Object obj)
+	{
+		if(field != null)
+		{
+			try
+			{
+				return (T)field.get(obj);
+			}
+			catch(Throwable e)
+			{
+				Log.ex(e);
+			}
+		}
+		return null;
+	}
+	
+	
+	public void set(Object obj, T value)
+	{
+		if(field != null)
+		{
+			try
+			{
+				field.set(obj, value);
+			}
+			catch(Throwable e)
+			{
+				Log.ex(e);
+			}
+		}
+	}
+}
